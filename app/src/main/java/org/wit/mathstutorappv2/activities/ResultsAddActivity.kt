874562c4bdbb41bc889f026bc94426d94431f42d
GergSzla <44243377+GergSzla@users.noreply.github.com>
2019-05-01 17:53:49 +0100

    Validation Update

--- app/src/main/java/org/wit/mathstutorappv2/activities/ResultsAddActivity.kt
+++ app/src/main/java/org/wit/mathstutorappv2/activities/ResultsAddActivity.kt
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_mta.toolbarAdd
 import kotlinx.android.synthetic.main.activity_results_add.*
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.toast
 import org.wit.mathstutorappv2.R
 import org.wit.mathstutorappv2.main.MainApp
 import org.wit.mathstutorappv2.models.MTAModel
@@ -32,22 +33,36 @@ class ResultsAddActivity : AppCompatActivity(), AnkoLogger {
          */
         btnAddScore.setOnClickListener {
                var lastSessionScore = txtScore.text.toString()
-                statss.sessionsTaken += 1           //increments sessionsTaken as a new session has just been finished
 
-                if(lastSessionScore.toInt() > 4 ){  //if the last session score is greater than 4, session was passed
+
+                if(lastSessionScore.toInt() in 5..10){  //if the last session score is greater than 4, session was passed
+                    statss.sessionsTaken += 1           //increments sessionsTaken as a new session has just been finished
                     statss.sessionsPassed += 1      //increments sessionsPassed by 1 each time the last session was passed
 
+                    statss.totalQuestionsAnswered +=10      //each challenge contains 10 questions thus total questions/session increments by 10
+                    statss.totalAnsweredCorrect += lastSessionScore.toInt() //increments by the users inserted score
+                    statss.totalAnsweredWrong += (10-lastSessionScore.toInt()) // increments totalAnsweredWrong by the remainder of (10(questions) minus the
+                    //amount of questions answered correctly
+
+                    app.stats.saveStats(statss)             //saves stats to json file
+                    finish()
+
                 } else if(lastSessionScore.toInt() <= 4){ //if the last session score is less than or = 4, session was failed
+                    statss.sessionsTaken += 1           //increments sessionsTaken as a new session has just been finished
                     statss.sessionsFailed += 1            //increments sessionsFailed by 1 each time a session was failed
+
+                    statss.totalQuestionsAnswered +=10      //each challenge contains 10 questions thus total questions/session increments by 10
+                    statss.totalAnsweredCorrect += lastSessionScore.toInt() //increments by the users inserted score
+                    statss.totalAnsweredWrong += (10-lastSessionScore.toInt()) // increments totalAnsweredWrong by the remainder of (10(questions) minus the
+                    //amount of questions answered correctly
+
+                    app.stats.saveStats(statss)             //saves stats to json file
+                    finish()
+
+                }else if(lastSessionScore.toInt()< 0 || lastSessionScore.toInt() > 10 ){
+                    toast("ERROR: $lastSessionScore out of bounds! Score must be between 0 and 10 \nPlease try again.")
+                    finish()
                 }
-
-                statss.totalQuestionsAnswered +=10      //each challenge contains 10 questions thus total questions/session increments by 10
-                statss.totalAnsweredCorrect += lastSessionScore.toInt() //increments by the users inserted score
-                statss.totalAnsweredWrong += (10-lastSessionScore.toInt()) // increments totalAnsweredWrong by the remainder of (10(questions) minus the
-                                                                            //amount of questions answered correctly
-
-                app.stats.saveStats(statss)             //saves stats to json file
-                finish()
         }
     }
 }
